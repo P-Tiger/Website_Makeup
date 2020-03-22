43538facd00d846b2a25f3ab5bfb92df8a14360c
Peter Parker <thivvnlh@gmail.com>
2020-03-22 20:44:58 +0700

    click icon search

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+var click = 0;
+function showSearch() {
+    click++;
+    if(click%2 != 0){
+        document.getElementById("click_search").style.display = 'flex';
+    }else{
+        document.getElementById("click_search").style.display = 'none';
+    }
+}
+
