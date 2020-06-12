25056fed27237e03352bd788ac3971539a9f73a0
Peter Parker <thivvnlh@gmail.com>
2020-06-13 03:34:29 +0700

    add Js & add Event

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,47 @@
-let click = 0;
+//  show Search Mobile
+$(document).ready(function () {
+    $('#logout').on('click', function () {
+        let avatar = $(this).parent().parent();
+        let logInAndRegister = $('.header__nav-item.header__nav-item--bold')
+        if (avatar.attr('id') === 'show') {
+            avatar.attr('id', 'hide');
+            logInAndRegister.attr('id', 'show');
+        }
+    })
+
+    $('.header__nav-item--sparate').on('click', function () {
+        $('.modal').css('display', 'flex');
+        $('.register').attr('id', '');
+    })
+
+    $('.header__nav-item--login').on('click', function () {
+        $('.modal').css('display', 'flex');
+        $('.login').attr('id', '');
+    })
+
+    $('.auth-form__switch-btn--login').on('click', function () {
+        $('.register').attr('id', 'hide');
+        $('.login').attr('id', '');
+    })
+
+    $('.auth-form__switch-btn--register').on('click', function () {
+        $('.register').attr('id', '');
+        $('.login').attr('id', 'hide');
+    })
+
+    $('.auth-form__controll-back').on('click', function () {
+        $('.modal').css('display', 'none');
+        $('.login').attr('id', 'hide');
+        $('.register').attr('id', 'hide');
+    })
+})
 
 function showSearch() {
-    click++;
-    if (click % 2 != 0) {
-        document.getElementById("click_search").style.display = 'flex';
+    let clickSearch = $('#click_search');
+
+    if (clickSearch.css('display') === 'none') {
+        clickSearch.css('display', 'flex');
     } else {
-        document.getElementById("click_search").style.display = 'none';
+        clickSearch.css('display', 'none');
     }
 }
-
